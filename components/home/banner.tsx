e87e50c02d3a04adb0922e422c7dbc00ce3677a6
agredo-alejo <agredoalejo@gmail.com>
2023-03-21 12:22:29 -0400

    banner from svg to png

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function Banner() {
    return (
-      <div className="flex items-center justify-center w-full py-0 bg-cover">
+      <div className="flex items-center justify-center w-full py-0 ">
          <Image
-            src="/NewSvg.svg"
+            src="/NewSvg.png"
             alt=""
             fill
             className="!relative !w-full !h-auto  drop-shadow-md"
